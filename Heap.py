0b33df3519c9be9f1e4fa07896627fc465eb189a
ngure <johnwngure@gmail.com>
2023-02-06 16:33:55 +0300

    heap2

--- Heap.py
+++ Heap.py
@@ -2,11 +2,11 @@ import heapq
 
 heap = []
 
-heapq.heappush(heap, 10)
+heapq.heappush(heap, 20)
 heapq.heappush(heap, 70)
-heapq.heappush(heap, 5)
+heapq.heappush(heap, 10)
 heapq.heappush(heap, 35)
 heapq.heappush(heap, 50)
 
-minimum = heapq.heappop(heap)
-print(minimum)
+maximum = heapq.heappop(heap)
+print(maximum)
