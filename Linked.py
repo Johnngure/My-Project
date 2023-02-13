2bebf2facb95636b532da614335484379b195593
johnngure <johnwngure@gmail.com>
2023-02-13 06:39:10 +0300

    append node
    Signed-off-by: johnngure <johnwngure@gmail.com>

---
+++ Linked.py
@@ -0,0 +1,10 @@
+class Node:
+    def_init_(self, data):
+    self.data = data
+    self.next : None
+
+    class LinkedList:
+        def_init_(self):
+        self.head = None
+         def append(self, data):
+             new_node = Node(data)
