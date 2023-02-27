cefcbe3c8f13a528c3998cedb98c684793c41aca
johnngure <johnwngure@gmail.com>
2023-02-27 13:17:01 +0300

    stack
    Signed-off-by: johnngure <johnwngure@gmail.com>

--- c.py
+++ c.py
@@ -12,3 +12,9 @@ class mystack:
 
     def size(self):
         return self.stack_size
+
+f __name__ == "__main__" :
+    stack_obj = MyStack()
+    print("is_empty(): " + str(stack_obj.is_empty()))
+    print("peek(): " + str(stack_obj.peek()))
+    print("size(): " + str(stack_obj.size()))
