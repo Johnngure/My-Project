b316e65ec7980bfc592c3159e140d826f0a486ce
johnngure <johnwngure@gmail.com>
2023-03-03 23:04:22 +0300

    binary insert 2
    Signed-off-by: johnngure <johnwngure@gmail.com>

--- bin insertion.py
+++ bin insertion.py
@@ -14,22 +14,30 @@ def _display_aux(node):
     Returns list of strings, width, height,
     and horizontal coordinate of the root.
     """
-    # No child.
-    if node.rightChild is None and node.leftChild is None:
-        line = str(node.val)
-        width = len(line)
-        height = 1
-        middle = width // 2
-        return [line], width, height, middle
+  # Two children.
+    left, n, p, x = _display_aux(node.leftChild)
+    right, m, q, y = _display_aux(node.rightChild)
+    s = '%s' % node.val
+    u = len(s)
+    first_line = (x + 1) * ' ' + (n - x - 1) * \
+        '_' + s + y * '_' + (m - y) * ' '
+    second_line = x * ' ' + '/' + \
+        (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+    if p < q:
+        left += [n * ' '] * (q - p)
+    elif q < p:
+        right += [m * ' '] * (p - q)
+    zipped_lines = zip(left, right)
+    lines = [first_line, second_line] + \
+        [a + u * ' ' + b for a, b in zipped_lines]
+    return lines, n + m + u, max(p, q) + 2, n + u // 2
 
-    # Only left child.
-    if node.rightChild is None:
-        lines, n, p, x = _display_aux(node.leftChild)
-        s = str(node.val)
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-        second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-        shifted_lines = [line + u * ' ' for line in lines]
-        final_lines = [first_line, second_line] + shifted_lines
-        return final_lines, n + u, p + 2, n + u // 2
 
+BST = BinarySearchTree(50)
+for _ in range(15):
+    ele = random.randint(0, 100)
+    print("Inserting "+str(ele)+":")
+    BST.insert(ele)
+    # We have hidden the code for this function but it is available for use!
+    display(BST.root)
+    print('\n')
