c38fb03643d65340a3b73e1744192a2742656056
johnngure <johnwngure@gmail.com>
2023-03-04 02:07:41 +0300

    snipet for delete.BST
    Signed-off-by: johnngure <johnwngure@gmail.com>

---
+++ delete val.py
@@ -0,0 +1,7 @@
+def delete(self, val):
+    if val < self.val:  # val is in the left subtree
+        pass
+    elif val > self.val:  # val is in the right subtree
+        pass
+    else:  # val was found
+        pass
