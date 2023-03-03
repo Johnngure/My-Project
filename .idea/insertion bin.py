3cf971173389aad155db12c696ee5a1d85855099
johnngure <johnwngure@gmail.com>
2023-03-03 23:14:35 +0300

    binary insertion recursive
    Signed-off-by: johnngure <johnwngure@gmail.com>

---
+++ .idea/insertion bin.py
@@ -0,0 +1,13 @@
+from Node import Node
+
+
+class BinarySearchTree:
+    def __init__(self, val):
+        self.root = Node(val)
+
+    def insert(self, val):
+        if self.root:
+            return self.root.insert(val)
+        else:
+            self.root = Node(val)
+            return True
