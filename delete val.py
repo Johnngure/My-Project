5798ffda9a0481cce26af25191fb6e1473eb4433
johnngure <johnwngure@gmail.com>
2023-03-04 03:48:23 +0300

    delete val when found
    Signed-off-by: johnngure <johnwngure@gmail.com>

--- delete val.py
+++ delete val.py
@@ -4,4 +4,21 @@ def delete(self, val):
     elif val > self.val:  # val is in the right subtree
         pass
     else:  # val was found
-        pass
+        if self.righchild  and self.leftchild is None:
+            self = None
+            return None
+        elif self.leftchild is None:
+            tmp = self.rightchild
+            self = None
+            return tmp
+        else:
+            current = self.rightchild
+            # loop down to find the leftmost leaf
+            while (current.leftchild is not None):
+                current = current.leftchild
+                self.val = current.val
+                self.rightchild = self.rightchild.delete(current.val)
+
+return self
+
+
